ed4ad0f0f501913fe8fa21ac28644a49b3197ee1
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2024-01-17 22:16:55 +0100

    we are happy, we commit

--- loreal_project/frontend/src/pages/Home/Home.jsx
+++ loreal_project/frontend/src/pages/Home/Home.jsx
@@ -20,16 +20,28 @@ function Home() {
       </div>
       <div className="globalproduct">
         <div className="product1">
-            <img src="../../assets/skincare.jpg" alt="skincare_product"/>
+          <img
+            src="src\assets\skincare.jpg"
+            alt="skincare_product"
+            height="200"
+          />
         </div>
         <div className="product2">
-            <img src="../../assets/makeup.jpg" alt="makeup_product"/>
+          <img src="src\assets\makeup.jpg" alt="makeup_product" height="300" />
         </div>
         <div className="product3">
-            <img src="../../assets/haircolor.jpg" alt="haircolor_product"/>
+          <img
+            src="src\assets\haircolor.jpg"
+            alt="haircolor_product"
+            height="300"
+          />
         </div>
-        <div className="product3">
-            <img src="../../assets/shampoo.jpg" alt="shampoo_product"/>
+        <div className="product4">
+          <img
+            src="src\assets\shampoo.jpg"
+            alt="shampoo_product"
+            height="300"
+          />
         </div>
       </div>
     </div>
