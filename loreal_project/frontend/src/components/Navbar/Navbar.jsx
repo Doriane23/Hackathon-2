5769be7f6abeeee25a78341c9fbc5c6a5649bf00
Doriane LEVEQUE <doriane.leveque@hotmail.fr>
2024-01-17 12:53:34 +0100

    Init Navbar

---
+++ loreal_project/frontend/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,11 @@
+import "./Navbar.scss";
+
+function Navbar() {
+    return (
+        <nav>
+            <div>
+                <h1>Je suis la NAVBAR</h1>
+            </div>
+        </nav>
+    )
+}
