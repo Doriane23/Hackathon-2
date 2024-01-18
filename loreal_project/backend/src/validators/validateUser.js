3a2a6d868689c9bc44a423046fe60ae8c247a3dc
diogo596 <diogo596@hotmail.fr>
2024-01-18 09:51:18 +0100

    add-item

--- loreal_project/backend/src/validators/validateUser.js
+++ loreal_project/backend/src/validators/validateUser.js
@@ -3,20 +3,19 @@ const Joi = require("joi");
 const getUserSchema = () => {
   return Joi.object({
     id: Joi.number().presence("optional"),
-    id: Joi.number().presence('required'),
-   firstname: Joi.string().max(50).presence('required'),
-   lastname: Joi.string().max(50).presence('required'),
-   email: Joi.string().max(250).presence('required'),
-   password: Joi.string().max(250).presence('required'),
-   profil: Joi.string().max(50).presence('required'),
-   adress: Joi.string().max(250).presence('required'),
-   age: Joi.number().presence('required'),
-   hair_type: Joi.string().max(50).presence('required'),
-   hair_color: Joi.string().max(50).presence('required'),
-   skin_type: Joi.string().max(50).presence('required'),
-   skin_color: Joi.string().max(50).presence('required'),
-   id_products: Joi.number().presence('optional'),
-
+    id: Joi.number().presence("required"),
+    firstname: Joi.string().max(50).presence("required"),
+    lastname: Joi.string().max(50).presence("required"),
+    email: Joi.string().max(250).presence("required"),
+    password: Joi.string().max(250).presence("required"),
+    profil: Joi.string().max(50).presence("required"),
+    adress: Joi.string().max(250).presence("required"),
+    age: Joi.number().presence("required"),
+    hair_type: Joi.string().max(50).presence("required"),
+    hair_color: Joi.string().max(50).presence("required"),
+    skin_type: Joi.string().max(50).presence("required"),
+    skin_color: Joi.string().max(50).presence("required"),
+    id_products: Joi.number().presence("optional"),
   });
 };
 
@@ -38,4 +37,3 @@ const validateUser = (req, res, next) => {
 };
 
 module.exports = validateUser;
-
--- loreal_project/frontend/src/components/Login/Login.jsx
+++ loreal_project/frontend/src/components/Login/Login.jsx
@@ -29,25 +29,7 @@ function FormRegister({ isLogin, modal }) {
               </ul>
             </div>
             <div className="field">
-              <svg
-                className="input-icon"
-                version="1.1"
-                id="Layer_1"
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                width="16px"
-                height="16px"
-                viewBox="0 0 50 50"
-                fill="white"
-              >
-                {" "}
-                <path
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#ffffff"
-                  d="M 21.5,2.5 C 29.1495,1.65036 34.1495,4.81702 36.5,12C 36.094,18.3705 34.4273,24.3705 31.5,30C 31.7591,31.2613 32.4258,32.2613 33.5,33C 36.5833,34.1392 39.5833,35.4725 42.5,37C 44.5744,39.3016 45.9077,41.9683 46.5,45C 32.5,45.6667 18.5,45.6667 4.5,45C 5.09231,41.9683 6.42565,39.3016 8.5,37C 12.232,35.5393 15.732,33.7059 19,31.5C 17.0747,26.4527 15.908,21.1194 15.5,15.5C 14.0907,9.32811 16.0907,4.99478 21.5,2.5 Z M 22.5,5.5 C 32.2038,5.32049 35.2038,9.65382 31.5,18.5C 31.8333,19.1667 32.1667,19.8333 32.5,20.5C 25.4721,30.1208 27.8054,36.2874 39.5,39C 40.6872,40.0195 41.6872,41.1861 42.5,42.5C 31.1667,43.8333 19.8333,43.8333 8.5,42.5C 9.31277,41.1861 10.3128,40.0195 11.5,39C 23.1946,36.2874 25.5279,30.1208 18.5,20.5C 19.0805,18.1127 19.0805,15.6127 18.5,13C 17.9574,9.4027 19.2908,6.9027 22.5,5.5 Z"
-                />
-              </svg>
+
               <input
                 autoComplete="off"
                 placeholder="Firstname"
@@ -56,25 +38,7 @@ function FormRegister({ isLogin, modal }) {
               />
             </div>
             <div className="field">
-              <svg
-                className="input-icon"
-                version="1.1"
-                id="Layer_1"
-                xmlns="http://www.w3.org/2000/svg"
-                x="0px"
-                y="0px"
-                width="16px"
-                height="16px"
-                viewBox="0 0 50 50"
-                fill="white"
-              >
-                {" "}
-                <path
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="#ffffff"
-                  d="M 21.5,2.5 C 29.1495,1.65036 34.1495,4.81702 36.5,12C 36.094,18.3705 34.4273,24.3705 31.5,30C 31.7591,31.2613 32.4258,32.2613 33.5,33C 36.5833,34.1392 39.5833,35.4725 42.5,37C 44.5744,39.3016 45.9077,41.9683 46.5,45C 32.5,45.6667 18.5,45.6667 4.5,45C 5.09231,41.9683 6.42565,39.3016 8.5,37C 12.232,35.5393 15.732,33.7059 19,31.5C 17.0747,26.4527 15.908,21.1194 15.5,15.5C 14.0907,9.32811 16.0907,4.99478 21.5,2.5 Z M 22.5,5.5 C 32.2038,5.32049 35.2038,9.65382 31.5,18.5C 31.8333,19.1667 32.1667,19.8333 32.5,20.5C 25.4721,30.1208 27.8054,36.2874 39.5,39C 40.6872,40.0195 41.6872,41.1861 42.5,42.5C 31.1667,43.8333 19.8333,43.8333 8.5,42.5C 9.31277,41.1861 10.3128,40.0195 11.5,39C 23.1946,36.2874 25.5279,30.1208 18.5,20.5C 19.0805,18.1127 19.0805,15.6127 18.5,13C 17.9574,9.4027 19.2908,6.9027 22.5,5.5 Z"
-                />
-              </svg>
+
               <input
                 autoComplete="off"
                 placeholder="LASTNAME"
@@ -83,18 +47,7 @@ function FormRegister({ isLogin, modal }) {
               />
             </div>
             <div className="field">
-              <svg
-                className="input-icon"
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path d="M13.106 7.222c0-2.967-2.249-5.032-5.482-5.032-3.35 0-5.646 2.318-5.646 5.702 0 3.493 2.235 5.708 5.762 5.708.862 0 1.689-.123 2.304-.335v-.862c-.43.199-1.354.328-2.29.328-2.926 0-4.813-1.88-4.813-4.798 0-2.844 1.921-4.881 4.594-4.881 2.735 0 4.608 1.688 4.608 4.156 0 1.682-.554 2.769-1.416 2.769-.492 0-.772-.28-.772-.76V5.206H8.923v.834h-.11c-.266-.595-.881-.964-1.6-.964-1.4 0-2.378 1.162-2.378 2.823 0 1.737.957 2.906 2.379 2.906.8 0 1.415-.39 1.709-1.087h.11c.081.67.703 1.148 1.503 1.148 1.572 0 2.57-1.415 2.57-3.643zm-7.177.704c0-1.197.54-1.907 1.456-1.907.93 0 1.524.738 1.524 1.907S8.308 9.84 7.371 9.84c-.895 0-1.442-.725-1.442-1.914z">
-                  {" "}
-                </path>
-              </svg>
+
               <input
                 autoComplete="off"
                 placeholder="Email"
@@ -103,18 +56,7 @@ function FormRegister({ isLogin, modal }) {
               />
             </div>
             <div className="field">
-              <svg
-                className="input-icon"
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z">
-                  {" "}
-                </path>
-              </svg>
+
               <input
                 placeholder="Password"
                 className="input-field"
@@ -122,22 +64,65 @@ function FormRegister({ isLogin, modal }) {
               />
             </div>
             <div className="field">
-              <svg
-                className="input-icon"
-                xmlns="http://www.w3.org/2000/svg"
-                width="16"
-                height="16"
-                fill="currentColor"
-                viewBox="0 0 16 16"
-              >
-                <path d="M8 1a2 2 0 0 1 2 2v4H6V3a2 2 0 0 1 2-2zm3 6V3a3 3 0 0 0-6 0v4a2 2 0 0 0-2 2v5a2 2 0 0 0 2 2h6a2 2 0 0 0 2-2V9a2 2 0 0 0-2-2z">
-                  {" "}
-                </path>
-              </svg>
+
               <input
-                placeholder="Confirm password"
+                placeholder="Profil"
                 className="input-field"
                 type="password"
+              />
+            </div>
+            <div className="field">
+
+              <input
+                autoComplete="off"
+                placeholder="Address"
+                className="input-field"
+                type="text"
+              />
+            </div>
+            <div className="field">
+
+              <input
+                autoComplete="off"
+                placeholder="Age"
+                className="input-field"
+                type="text"
+              />
+            </div>
+            <div className="field">
+
+              <input
+                autoComplete="off"
+                placeholder="Hair_type"
+                className="input-field"
+                type="text"
+              />
+            </div>
+            <div className="field">
+
+              <input
+                autoComplete="off"
+                placeholder="Hair_color"
+                className="input-field"
+                type="text"
+              />
+            </div>
+            <div className="field">
+
+              <input
+                autoComplete="off"
+                placeholder="Skin_type"
+                className="input-field"
+                type="text"
+              />
+            </div>
+            <div className="field">
+
+              <input
+                autoComplete="off"
+                placeholder="Skin_color"
+                className="input-field"
+                type="text"
               />
             </div>
             <div className="btn2">
